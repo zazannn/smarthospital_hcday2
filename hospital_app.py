68d87e932be3306f456722f18759d90a29d35eb5
zazannn <rizan@kodingnext.com>
2026-06-27 10:28:10 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -5,3 +5,22 @@ import pickle
 st.set_page_config(page_title="Smart Hospital Patient Navigator", page_icon="🏥")
 
 st.title("🏥 Smart Hospital Navigator")
+
+@st.cache_resource
+def load_model():
+  with open("hospital_model.pkl", "rb") as f:
+    return pickle.load(f)
+
+bundle = load_model()
+
+model = bundle['model']
+scaler = bundle['scaler']
+features = bundle['features']
+cols_to_scale = bundle['cols_to_scale']
+dept_map_inv = bundle['dept_map_inv']
+gender_map = bundle['gender_map']
+temp_map = bundle['temp_map']
+hr_map = bundle['hr_map']
+dur_map = bundle['dur_map']
+cc_map = bundle['cc_map']
+
